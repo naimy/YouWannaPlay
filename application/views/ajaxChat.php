1cd13abb4db31485f5af4d69a4396c9ca0c742e1
naimy <jonat.palumbo@gmail.com>
2015-07-05 13:19:46 +0200

    reset

--- application/views/ajaxChat.php
+++
@@ -1,3 +0,0 @@
-<?php foreach($chats as $chat){?>
-<p><?php echo $chat->date; ?> - <?php echo date('H:i:s', $chat->time); ?> - <?php echo $chat->name; ?> : <?php echo $chat->content; ?></p>
-<?php } ?>
