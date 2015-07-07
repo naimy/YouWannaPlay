b8c889fcf700229dd1358cca9dcfccaab170b490
naimy <jonat.palumbo>
2015-07-07 11:13:47 +0200

    test youtuve api

--- application/controllers/video.php
+++ application/controllers/video.php
@@ -3,9 +3,27 @@ if (! defined ( 'BASEPATH' ))
 	exit ( 'No direct script access allowed' );
 class Video extends MY_Controller {
 	public function index() {
+		
 		$this->data['SelectedMenu'] = 'video';
 		$this->data ['title'] = 'Vidéos';
 		$this->content = 'video'; // passing middle to function. change this for different views.
+		
+		require ('/public_html/api/vendor/autoload.php');
+		$this->data['APIKey'] = 'AIzaSyCEdA2n4j2ZlfqkrEA86I3Abp1p3jU0wbI';
+		
+		$client = new Google_Client();
+		$client->setDeveloperKey($this->data['APIKey']);
+		
+		$youtube = new Google_Service_Youtube($client);
+		
+		$response = $youtube->search->listSearch('id,snippet',['q' => 'racoon'], 'order'=> 'relevance','maxResults' => 10, 'type'=>'video');
+		
+		echo '<pre>';
+		var_dump($response);
+		echo '</pre>';
+		
+		die();
+				
 		$this->layout ();
 	}
 }
--- application/views/video.php
+++ application/views/video.php
@@ -1,8 +1,3 @@
-<h2>Zappings</h2>
-<iframe width="940" height="450" src="https://www.youtube.com/embed/videoseries?list=PL2ftsrGjewJqV6iyH_dlNTG9DL0GOTJvN" frameborder="0" allowfullscreen></iframe>
+<?php 
 
-<h2>Les petits +</h2>
-<iframe width="940" height="450" src="https://www.youtube.com/embed/jfzPU0mYZOY?list=PL2ftsrGjewJqMGAn0ZNcgUn0Ndqrb_xYK" frameborder="0" allowfullscreen></iframe>
 
-<h2>Scoop Mamaoue</h2>
-<iframe width="940" height="450" src="https://www.youtube.com/embed/52CUSl3-FVs?list=PL2ftsrGjewJrUjBqaEU4ynx9uKJYx7qh3" frameborder="0" allowfullscreen></iframe>
