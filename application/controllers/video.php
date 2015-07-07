c113de662296481f2d4927122901918e716245b6
naimy <jonat.palumbo>
2015-07-07 11:25:29 +0200

    youtube test

--- application/controllers/video.php
+++ application/controllers/video.php
@@ -1,7 +1,8 @@
 <?php
-if (! defined ( 'BASEPATH' ))
-	exit ( 'No direct script access allowed' );
+if (! defined ( 'BASEPATH' )) exit ( 'No direct script access allowed' );
+
 class Video extends MY_Controller {
+	
 	public function index() {
 		
 		$this->data['SelectedMenu'] = 'video';
@@ -9,14 +10,21 @@ class Video extends MY_Controller {
 		$this->content = 'video'; // passing middle to function. change this for different views.
 		
 		require ('/public_html/api/vendor/autoload.php');
-		$this->data['APIKey'] = 'AIzaSyCEdA2n4j2ZlfqkrEA86I3Abp1p3jU0wbI';
 		
 		$client = new Google_Client();
-		$client->setDeveloperKey($this->data['APIKey']);
+		$client->setDeveloperKey('AIzaSyCEdA2n4j2ZlfqkrEA86I3Abp1p3jU0wbI');
 		
 		$youtube = new Google_Service_Youtube($client);
 		
-		$response = $youtube->search->listSearch('id,snippet',['q' => 'racoon', 'order' => 'relevance','maxResults' => 10, 'type' => 'video']);
+		$response = $youtube->search->listSearch(
+				'id,snippet',
+				[
+					'q' => 'racoon',
+			 		'order' => 'relevance',
+					'maxResults' => 10,
+					'type' => 'video'
+				]
+		);
 		
 		echo '<pre>';
 		var_dump($response);
@@ -26,4 +34,5 @@ class Video extends MY_Controller {
 				
 		$this->layout ();
 	}
+	
 }
