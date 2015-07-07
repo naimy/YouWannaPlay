7dba3fce548232f8e25b18ed9c680224247e5313
naimy <jonat.palumbo>
2015-07-07 11:36:10 +0200

    test

--- application/controllers/video.php
+++ application/controllers/video.php
@@ -9,9 +9,9 @@ class Video extends MY_Controller {
 		$this->data ['title'] = 'Vidéos';
 		$this->content = 'video'; // passing middle to function. change this for different views.
 		
-		var_dump(phpinfo());
+		var_dump(HTTP_HOST.'/public_html/api/vendor/autoload.php');
 		
-		require (BASEPATH.'/public_html/api/vendor/autoload.php');
+		require (HTTP_HOST.'/public_html/api/vendor/autoload.php');
 		
 		$client = new Google_Client();
 		$client->setDeveloperKey('AIzaSyCEdA2n4j2ZlfqkrEA86I3Abp1p3jU0wbI');
